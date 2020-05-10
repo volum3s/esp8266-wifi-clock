feae6aac6cd31ab73e3a8c0c54a675ed6b5f717f
volum3s <volum3s@pantsoffsecurity.com>
2020-05-10 14:56:22 -0700

    Added weather, pulled with WifiClientSecure. Loops use millis instead of delay now. Moved wifi info, coordinates, and API key to separate credentials.h file.

--- esp8266-wifi-clock.ino
+++ esp8266-wifi-clock.ino
@@ -2,20 +2,17 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <LiquidCrystal.h>
-#include <HTTPClient.h>
+#include <WifiClientSecure.h>
+#include <ArduinoJson.h>
+#include "credentials.h"
 
 // Set up LiquidCrystal library with  LCD1602 pins
 // Pinout from LCD1602 is (RS, E, D4, D5, D6, D7)
 LiquidCrystal lcd(12, 14, 2, 0, 4, 5);
 
-const char *ssid     = "ENTER_SSID_HERE";
-const char *password = "ENTER_PASS_HERE";
-
-// Configuring DarkSky API (Add your API key and coordinates below)
-const String apiKey = "<INSERT API KEY HERE>/"
-const String lat = "<INSERT LATITUDE HERE>,"
-const String lon = "<INSERT LONGITUDE HERE>"
-const String url = "https://api.darksky.net/forecast/"
+// Configuring DarkSky API connection
+const String blockParams = "?exclude=minutely,hourly,daily,alerts,flags";
+const char* host = "darksky.net";
 
 // This offset is for UTC-7
 const long utcOffsetInSeconds = -25200;
@@ -24,6 +21,12 @@ char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+// Using millis instead of delay. Different delays for time and weather loops.
+#define NTP_INTERVAL 1000
+#define WEATHER_INTERVAL 30000
+unsigned long ntpTimer = 0;
+unsigned long weatherTimer = 0;
 
 void setup(){
   Serial.begin(115200);
@@ -38,19 +41,18 @@ void setup(){
   delay(3000);
   lcd.clear();
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   lcd.setCursor(0,0);
   lcd.print("Connecting to:");
   lcd.setCursor(0,1);
-  lcd.print(ssid);
+  lcd.print(WIFI_SSID);
 
   while ( WiFi.status() != WL_CONNECTED ) {
-    // Serial.print ( "." );
     lcd.setCursor(15, 1);
     lcd.print("\\");
     delay(200);
     lcd.setCursor(15, 1);
-    lcd.print("|");
+    lcd.print("\|");
     delay(200);
     lcd.setCursor(15, 1);
     lcd.print("/");
@@ -60,40 +62,78 @@ void setup(){
   lcd.print("Wi-Fi Connected!");
   lcd.setCursor(0,1);
   lcd.print("IP: " + WiFi.localIP());
-  delay(3000);
+  delay(2000);
   
   timeClient.begin();  
   lcd.clear();
 }
 
 void loop() {
-  timeClient.update();
-  lcd.setCursor(0, 0);
+  // Get time
+  if(millis() >= ntpTimer + NTP_INTERVAL){
+    ntpTimer +=NTP_INTERVAL;
+    timeClient.update();
+    lcd.setCursor(0, 0);
+    lcd.print(daysOfTheWeek[timeClient.getDay()]);
+    lcd.print(" ");
 
-  lcd.print("  ");
-  lcd.print(daysOfTheWeek[timeClient.getDay()]);
-  lcd.print(" ");
+    // Print HH:MM:SS, prepending a zero for single digits
+    if ((timeClient.getHours()) < 10) {
+      lcd.print("0");
+    }
+    lcd.print(timeClient.getHours());
+    lcd.print(":");
+    
+    if ((timeClient.getMinutes()) < 10) {
+      lcd.print("0");
+    }
+    lcd.print(timeClient.getMinutes());
+    lcd.print(":");
+    
+    if ((timeClient.getSeconds()) < 10) {
+      lcd.print("0");
+    }
+    lcd.print(timeClient.getSeconds());
+    lcd.print(" ");
+  }
+  
+  // Get weather
+  if(millis() >= weatherTimer + WEATHER_INTERVAL){
+    WiFiClientSecure client;
+    client.setInsecure();
+    lcd.setCursor(0, 1);
+  
+    if (!client.connect(host, 443))
+      lcd.print("Cant get weather");
 
-  // Print HH:MM:SS, prepending a zero for single digits
-  if ((timeClient.getHours()) < 10) {
-    lcd.print("0");
+    else {
+      lcd.print("Getting weather!");
+      // Make the HTTP request
+      client.println(String("GET https://api.") + host + "/forecast/" + DS_API_KEY + "/" + DS_LAT + "," + DS_LON + blockParams + " HTTP/1.0");
+      client.print("Host: ");
+      client.println(host);
+      client.println("Connection: close");
+      client.println();
+    }
+    
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        break;
+      }
+    }
+    
+    String line = client.readStringUntil('\n');
+    const size_t capacity = JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(19) + 340;
+    DynamicJsonDocument doc(capacity);    
+    deserializeJson(doc, line);
+    lcd.setCursor(13, 0);
+    lcd.print(doc["currently"]["temperature"].as<int>());
+    lcd.print("F");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    lcd.setCursor(0, 1);
+    lcd.print(doc["currently"]["summary"].as<String>());
+    weatherTimer +=WEATHER_INTERVAL;
   }
-  lcd.print(timeClient.getHours());
-  lcd.print(":");
-  
-  if ((timeClient.getMinutes()) < 10) {
-    lcd.print("0");
-  }
-  lcd.print(timeClient.getMinutes());
-  lcd.print(":");
-  
-  if ((timeClient.getSeconds()) < 10) {
-    lcd.print("0");
-  }
-  lcd.print(timeClient.getSeconds());
-  
-  lcd.setCursor(0,1);
-// Planning to add weather info here like "55F - Rain"
-
-  delay(1000);
 }
